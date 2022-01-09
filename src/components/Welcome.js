d6b66efe6a0fff6aba5627823794359d0171ec81
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-01-09 15:40:05 -0500

    componetfuncional

---
+++ src/components/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles/Welcome.css'
+function Welcome(props){
+
+
+    return (
+        <div className="container">
+        <div className="Fitness-User-Info">
+            <h1>Bienvenido al Sistema DevFanor!</h1>
+            <p>¡Hagamos ejercicio para que alguien gane!</p>
+        </div>
+    </div>
+
+
+    )
+
+
+}
+
+export default Welcome 
--- src/index.js
+++ src/index.js
@@ -6,18 +6,26 @@
 //container.appendChild(element)
 import React from 'react'
 import ReactDom from 'react-dom'
-
+import Welcome from './components/Welcome'
 import Card from './components/Card'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 const container = document.getElementById('root') 
-ReactDom.render(<Card 
+ReactDom.render(<div>
+                  <Welcome
+                  username="Fanor Choque Aguirre"
+                  
+                  />
+                  <Card 
 
-              usuario="Nombre de Usuario "
-              datos="Nombre, Apellido, Ciudad, Escuela"
-              avatar="https://res.cloudinary.com/dhbn0fate/image/upload/v1639032523/fanor/fanor.jpg"
-              leftColor="#A74CF2"
-              rightColor="#617BFB"
+                usuario="Nombre de Usuario "
+                datos="Nombre, Apellido, Ciudad, Escuela"
+                avatar="https://res.cloudinary.com/dhbn0fate/image/upload/v1639032523/fanor/fanor.jpg"
+                leftColor="#A74CF2"
+                rightColor="#617BFB"
 
-/>, container)
+                />
+
+
+</div>, container)
