c6248684652479e3112d1bab519acd8d07aa6c5c
Fanor Choque Aguirre <fanorarmandochoque@gmail.com>
2022-01-08 22:50:35 -0500

    ReactDom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//const element = document.createElement('h1')
+//element.innerText = 'Hola React FANOR'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//const container = document.getElementById('root')
+//container.appendChild(element)
+import React from 'react'
+import ReactDom from 'react-dom'
+const element = <h1>Hola React Fanor</h1>
+const container = document.getElementById('root') 
+ReactDom.render(element, container)
