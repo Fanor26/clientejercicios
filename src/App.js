1033cf2e1b87e0d93f9aff6d3d975fc67ebcc216
FANOR CHOQUE AGUIRRE <fanorarmandochoque@gmail.com>
2022-04-04 02:25:00 +0000

    gitpod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenido a React Fanor
+          Bienvenido a React Fanor Choque
         </p>
         <a
           className="App-link"
