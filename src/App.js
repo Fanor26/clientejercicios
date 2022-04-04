c492d6ef36eccedc75206b7b63b6f1a97636d996
FANOR CHOQUE AGUIRRE <fanorarmandochoque@gmail.com>
2022-04-04 07:28:56 +0000

    botton

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
    
-       
+       <button>SEMINADIO DE SISTEMAS 2022</button>
         <p>
           fanor choque Aguirre 2022
           {/*npm remove react-scripts
